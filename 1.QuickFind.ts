c2beb8afbf34243eb312527b1789387f84dd6251
Kingsley Don <tj971204@gmail.com>
2019-02-16 17:21:24 +0800

    :art: QuickFind

--- 1.QuickFind.ts
+++ 1.QuickFind.ts
@@ -1,6 +1,20 @@
 class QuickFindUF {
   private id: number[]
+
   constructor(n: number) {
-    this.id = Array.from({ length: n }, (_, index) => index)
+    this.id = Array.from({ length: n }, (val, index) => index)
+  }
+
+  connected = (p: number, q: number): boolean => this.id[p] === this.id[q]
+
+  union = (p: number, q:number): void => {
+    const pid: number = this.id[p]
+    const qid: number = this.id[q]
+
+    this.id.forEach((val, index) => {
+      if (val === pid) {
+        this.id[index] = qid
+      }
+    })
   }
 }
